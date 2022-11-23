5259c1e5820b16ee49254480a2249084ee39554a
Edgar <aedgar990@gmail.com>
2022-11-23 14:03:28 +0300

    server: controllers

--- controllers/bookingsControllers.js
+++ controllers/bookingsControllers.js
@@ -2,5 +2,12 @@ const Bookings = require('../models/bookingsModel')
 
 // POST a booking
 const createBooking = async (req, res) => {
-    const 
+    const {firstName, lastName, email, phoneNumber, occupation} = req.body
+
+    //add doc to db
+    try {
+        const booking = await Bookings.create({firstName, lastName, email, phoneNumber, occupation})
+    } catch (error) {
+        res.status(404).json({error: error.message})
+    }
 }
