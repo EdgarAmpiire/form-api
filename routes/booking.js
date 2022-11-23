b4b2f4f23c0482f917b4484107a526431618076b
Edgar <aedgar990@gmail.com>
2022-11-23 11:21:44 +0300

    server: creating routes

--- routes/booking.js
+++ routes/booking.js
@@ -8,16 +8,24 @@ router.get('/', (req, res) => {
 })
 
 // GET a single booking
-
+router.get('/:id', (req, res) => {
+    res.json({msg: "Get a single booking"})
+})
 
 // POST a booking
-
+router.post('/', (req, res) => {
+    res.json({msg: "Add a booking"})
+})
 
 // DELETE a booking
-
+router.delete('/:id', (req, res) => {
+    res.json({msg: "Delete a booking"})
+})
 
 // UPDATE a booking
-
+router.patch('/:id', (req, res) => {
+    res.json({msg: "Update a booking"})
+})
 
 
 module.exports = router
