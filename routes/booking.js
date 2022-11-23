789091d05cbab0a621fc37bab96825cae73bb238
Edgar <aedgar990@gmail.com>
2022-11-23 13:42:31 +0300

    server: models and adding to db

--- routes/booking.js
+++ routes/booking.js
@@ -14,12 +14,13 @@ router.get('/:id', (req, res) => {
 })
 
 // POST a booking
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     const {firstName, lastName, email, phoneNumber, occupation} = req.body
 
     // add to db
     try {
-        
+        const booking = await Bookings.create({firstName, lastName, email, phoneNumber, occupation})
+        res.status(200).json(booking)
         
     } catch (error) {
         res.status(404).json({error: error.message})
