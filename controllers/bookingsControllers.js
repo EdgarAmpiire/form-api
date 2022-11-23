dc1a187a2950aec9434c63e203d8a2108ab38811
Edgar <aedgar990@gmail.com>
2022-11-23 14:04:24 +0300

    server: controllers

--- controllers/bookingsControllers.js
+++ controllers/bookingsControllers.js
@@ -1,14 +1,23 @@
-const Bookings = require('../models/bookingsModel')
+const Bookings = require("../models/bookingsModel");
 
 // POST a booking
 const createBooking = async (req, res) => {
-    const {firstName, lastName, email, phoneNumber, occupation} = req.body
+  const { firstName, lastName, email, phoneNumber, occupation } = req.body;
 
-    //add doc to db
-    try {
-        const booking = await Bookings.create({firstName, lastName, email, phoneNumber, occupation})
-        res.status(200).json(booking)
-    } catch (error) {
-        res.status(404).json({error: error.message})
-    }
-}
+  //add doc to db
+  try {
+    const booking = await Bookings.create({
+      firstName,
+      lastName,
+      email,
+      phoneNumber,
+      occupation,
+    });
+    res.status(200).json(booking);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
+
+
+module.exports = {createBooking}
