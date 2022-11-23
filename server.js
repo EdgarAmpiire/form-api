e9557cf18a5e29d1161356cdc50ddaaabd1866d7
Edgar <aedgar990@gmail.com>
2022-11-23 11:20:03 +0300

    server: creating routes

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const bookingRoutes = require('./routes/booking')
 const app = express()
 
 // middleware
+app.use(express.json())
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
