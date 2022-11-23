4cc9a1ebb164cb174dbde17f628cc6d2b8971537
Edgar <aedgar990@gmail.com>
2022-11-23 11:15:13 +0300

    server: creating routes

--- routes/book.js
+++ routes/book.js
@@ -1,1 +1,23 @@
 const express = require('express')
+
+const router = express.Router()
+
+// GET all bookings
+router.get('/', (req, res) => {
+    res.json({msg: "Get all bookings"})
+})
+
+// GET a single booking
+
+
+// POST a booking
+
+
+// DELETE a booking
+
+
+// UPDATE a booking
+
+
+
+module.exports = router
